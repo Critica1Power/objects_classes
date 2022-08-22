aa9bae3321cedb01d4e1ec2f66ff54ba87ea56a6
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-23 00:35:18 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        System.out.println("Задание 1");
+        Author aPushkin = new Author("Aleksandr", "Pushkin");
+        Author fDostoevskiy = new Author("Fedor", "Dostoevskiy");
+        Book evgeniyOnegin = new Book("Evgeniy Onegin", 1833, aPushkin);
+        Book crimeAndPunishment = new Book("Prestyplenie i nakazanie", 1866, fDostoevskiy);
+
+
+        System.out.println(aPushkin);
+        System.out.println(fDostoevskiy);
+        System.out.println(evgeniyOnegin);
+        System.out.println(crimeAndPunishment);
+
+        crimeAndPunishment.setPublishDate(2022);
+        System.out.println();
+        System.out.println(crimeAndPunishment);
     }
 }
